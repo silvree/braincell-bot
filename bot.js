b88bcab3864bb6c3115a0c944bb76273356dfcdf
silvree <silvia99g@gmail.com>
2021-01-17 19:48:12 -0500

    First files

---
+++ bot.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.login(process.env.BOT_TOKEN);
+
+client.on('message', message => {
+    if (message.content === 'ping') {
+       message.reply('pong');
+    }
+});
