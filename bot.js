248790f9a53d8432704bb45e5ef1fdd416669c31
silvree <silvia99g@gmail.com>
2021-02-02 23:20:57 -0500

    added coinflip command

--- bot.js
+++ bot.js
@@ -11,4 +11,14 @@ client.on('message', message => {
     if (message.content === 'ping') {
        message.reply('pong');
     }
+
+    if (message.content === 'coinflip') {
+        var coin_bool = Math.random() < 0.5;
+        if(coin_bool) {
+            message.reply('heads');
+        }
+        else {
+            message.reply('tails');
+        }
+     }
 });
